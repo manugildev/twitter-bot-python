a64c1d1cbc1dbc5076730cd918d7e486aeda455b
ManuGil <madtriangl3@gmail.com>
2016-02-23 09:08:33 +0100

    Cleaning files...

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import time
 import threading
 import configuration
 import sys, getopt
-
 import helpers
 import twitter
 from datetime import datetime
@@ -10,94 +9,91 @@ from termcolor import colored
 
 filename = open("files/targetUsers.txt", "r")
 targetUsers = set(filename.read().split('\n'))
-
 followedUsers = helpers.getUsersFromFile(configuration.FILE_FOLLOWED_USERS)
 followQueue = targetUsers - followedUsers
 
-api = {}
-
 
 def followBot():
-    try:
-        print(colored("Starting Follow Bot: " + str(len(followQueue)), 'cyan', attrs=['bold']))
-        time.sleep(1)
-        for u in followQueue:
-            if u != "" and u != "\n":
-                twitter.followUser(u)
-                time.sleep(int(configuration.ONE_DAY_S / configuration.FOLLOWS_PER_DAY))
-        print(colored("No more users to follow!", 'red'))
-    except Exception as e:
-        print(e)
+	try:
+		print(colored("Starting Follow Bot: " + str(len(followQueue)), 'cyan', attrs=['bold']))
+		time.sleep(1)
+		for u in followQueue:
+			if u != "" and u != "\n":
+				twitter.followUser(u)
+				time.sleep(int(configuration.ONE_DAY_S / configuration.FOLLOWS_PER_DAY))
+		print(colored("No more users to follow!", 'red'))
+	except Exception as e:
+		print(e)
 
 
 def unfollowBot():
-    try:
-        while True:
-            unfollowQueue = helpers.getUsersFromFileWithDate(configuration.FILE_FOLLOWED_USERS,
-                                                             configuration.DAYS_TO_UNFOLLOW)
-            print(colored("Starting UnFollow Bot: " + str(len(unfollowQueue)), 'cyan', attrs=['bold']))
-            time.sleep(1)
-            for u in unfollowQueue:
-                if u != "" and u != "\n":
-                    twitter.unfollowUser(u)
-                    time.sleep(int(configuration.ONE_DAY_S / configuration.UNFOLLOWS_PER_DAY))
-            unfollowQueue = helpers.getUsersFromFileWithDate(configuration.FILE_FOLLOWED_USERS,
-                                                             configuration.DAYS_TO_UNFOLLOW)
-            if len(unfollowQueue) == 0:
-                print(colored("Sleeping one hour, no elements in the unfollowing queue.", 'red'))
-                time.sleep(60 * 60)
+	try:
+		while True:
+			unfollowQueue = helpers.getUsersFromFileWithDate(configuration.FILE_FOLLOWED_USERS,
+			                                                 configuration.DAYS_TO_UNFOLLOW)
+			print(colored("Starting UnFollow Bot: " + str(len(unfollowQueue)), 'cyan', attrs=['bold']))
+			time.sleep(1)
+			for u in unfollowQueue:
+				if u != "" and u != "\n":
+					twitter.unfollowUser(u)
+					time.sleep(int(configuration.ONE_DAY_S / configuration.UNFOLLOWS_PER_DAY))
+			unfollowQueue = helpers.getUsersFromFileWithDate(configuration.FILE_FOLLOWED_USERS,
+			                                                 configuration.DAYS_TO_UNFOLLOW)
+			if len(unfollowQueue) == 0:
+				print(colored("Sleeping one hour, no elements in the unfollowing queue.", 'red'))
+				time.sleep(60 * 60)
 
-    except Exception as e:
-        print(e)
+	except Exception as e:
+		print(e)
 
 
 def statsBot():
-    print(colored("Starting Stats Bot", 'cyan', attrs=['bold']))
-    time.sleep(1)
-    try:
-        while True:
-            now = datetime.now()
-            if now.hour == 0 and now.minute == 0:
-                twitter.getStats(configuration.TWITTER_USERNAME)
-            time.sleep(60)
-    except Exception as e:
-        print(e)
+	print(colored("Starting Stats Bot", 'cyan', attrs=['bold']))
+	time.sleep(1)
+	try:
+		while True:
+			now = datetime.now()
+			if now.hour == 0 and now.minute == 0:
+				twitter.getStats(configuration.TWITTER_USERNAME)
+			time.sleep(60)
+	except Exception as e:
+		print(e)
 
 
 def bot():
-    threading.Thread(target=followBot).start()
-    threading.Thread(target=unfollowBot).start()
-    threading.Thread(target=statsBot).start()
+	threading.Thread(target=followBot).start()
+	threading.Thread(target=unfollowBot).start()
+	threading.Thread(target=statsBot).start()
 
 
 def main(argv):
-    print(colored("======= TWITTER BOT =======", 'magenta', attrs=['bold']))
-    try:
-        opts, args = getopt.getopt(argv, "hf:u:d:", ["follows=", "unfollows=", "days="])
-    except getopt.GetoptError:
-        print('main.py -f <follows_per_day> -u <unfollows_per_days> -d <days>')
-        sys.exit(2)
+	print(colored("======= TWITTER BOT =======", 'magenta', attrs=['bold']))
+	try:
+		opts, args = getopt.getopt(argv, "hf:u:d:", ["follows=", "unfollows=", "days="])
+	except getopt.GetoptError:
+		print('main.py -f <follows_per_day> -u <unfollows_per_days> -d <days>')
+		sys.exit(2)
 
-    for opt, arg in opts:
-        if opt == '-h':
-            print('main.py -f <follows_per_day> -u <unfollows_per_days> -d <days>')
-            sys.exit()
-        elif opt in ("-f", "--follows"):
-            configuration.FOLLOWS_PER_DAY = int(arg)
-        elif opt in ("-u", "--unfollows"):
-            configuration.UNFOLLOWS_PER_DAY = int(arg)
-        elif opt in ("-d", "--days"):
-            configuration.DAYS_TO_UNFOLLOW = int(arg)
+	for opt, arg in opts:
+		if opt == '-h':
+			print('main.py -f <follows_per_day> -u <unfollows_per_days> -d <days>')
+			sys.exit()
+		elif opt in ("-f", "--follows"):
+			configuration.FOLLOWS_PER_DAY = int(arg)
+		elif opt in ("-u", "--unfollows"):
+			configuration.UNFOLLOWS_PER_DAY = int(arg)
+		elif opt in ("-d", "--days"):
+			configuration.DAYS_TO_UNFOLLOW = int(arg)
 
-    print(colored("------ Configuration ------", 'blue'))
-    print('Follows per day: ' + str(configuration.FOLLOWS_PER_DAY))
-    print('Unfollows per day: ' + str(configuration.UNFOLLOWS_PER_DAY))
-    print('Days to unfollow: ' + str(configuration.DAYS_TO_UNFOLLOW))
-    print(colored("---------------------------", 'blue'))
+	print(colored("------ Configuration ------", 'blue'))
+	print('Follows per day: ' + str(configuration.FOLLOWS_PER_DAY))
+	print('Unfollows per day: ' + str(configuration.UNFOLLOWS_PER_DAY))
+	print('Days to unfollow: ' + str(configuration.DAYS_TO_UNFOLLOW))
+	print(colored("---------------------------", 'blue'))
 
-    # Create Threads that start the different modules
-    bot()
+	# Create Threads that start the different modules
+	bot()
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	main(sys.argv[1:])
--- twitter.py
+++ twitter.py
@@ -11,42 +11,43 @@ auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+
 def updateStatus(msg):
-    api.update_status(status=msg)
+	api.update_status(status=msg)
 
 
 def followUser(user):
-    try:
-        api.create_friendship(screen_name=user)
-        appendToFile(configuration.FILE_FOLLOWED_USERS, getDate() + ", " + user)
-        print(colored(getTime() + " User followed: " + user, 'green'))
-    except Exception as e:
-        print(colored(e, 'red'))
+	try:
+		api.create_friendship(screen_name=user)
+		appendToFile(configuration.FILE_FOLLOWED_USERS, getDate() + ", " + user)
+		print(colored(getTime() + " User followed: " + user, 'green'))
+	except Exception as e:
+		print(colored(e, 'red'))
 
 
 def unfollowUser(user):
-    try:
-        api.destroy_friendship(screen_name=user)
-        appendToFile(configuration.FILE_UNFOLLOWED_USERS, getDate() + ", " + user)
-        print(colored(getTime() + " User unfollowed: " + user, 'red'))
-    except Exception as e:
-        print(colored(e, 'red'))
+	try:
+		api.destroy_friendship(screen_name=user)
+		appendToFile(configuration.FILE_UNFOLLOWED_USERS, getDate() + ", " + user)
+		print(colored(getTime() + " User unfollowed: " + user, 'red'))
+	except Exception as e:
+		print(colored(e, 'red'))
 
 
 def getStats(user):
-    result = api.get_user(screen_name=user)
-    following = str(result.friends_count)
-    followers = str(result.followers_count)
-    tweets = str(result.statuses_count)
-    favourites = str(result.favourites_count)
-    print(colored("===========================", 'cyan'))
-    print("STATS of @" + user)
-    print("---------------------------")
-    print("Time: " + getDate())
-    print("Tweets: " + tweets)
-    print("Following: " + following)
-    print("Followers: " + followers)
-    print("Favourites: " + favourites)
-    print(colored("===========================", 'cyan'))
-    appendToFile(configuration.FILE_STATS,
-                 "{}:{}:{}:{}:{}".format(getCurrentMillis(), tweets, following, followers, favourites))
+	result = api.get_user(screen_name=user)
+	following = str(result.friends_count)
+	followers = str(result.followers_count)
+	tweets = str(result.statuses_count)
+	favourites = str(result.favourites_count)
+	print(colored("===========================", 'cyan'))
+	print("STATS of @" + user)
+	print("---------------------------")
+	print("Time: " + getDate())
+	print("Tweets: " + tweets)
+	print("Following: " + following)
+	print("Followers: " + followers)
+	print("Favourites: " + favourites)
+	print(colored("===========================", 'cyan'))
+	appendToFile(configuration.FILE_STATS,
+	             "{}:{}:{}:{}:{}".format(getCurrentMillis(), tweets, following, followers, favourites))
