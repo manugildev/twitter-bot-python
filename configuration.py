5a334e8983d3ed575a565e49214e05a200e02117
ManuGil <madtriangl3@gmail.com>
2016-02-23 09:08:02 +0100

    Configuration explained

--- configuration.py
+++ configuration.py
@@ -1,18 +1,21 @@
-keys = {"API_KEY": "=========================",
-        "API_SECRET": "=========================",
-        "ACCESS_TOKEN": "=====================================",
-        "ACCESS_TOKEN_SECRET": "====================================="}
+# TWITTER KEYS
+keys = {"API_KEY"            : "=====================",
+        "API_SECRET"         : "==================================================",
+        "ACCESS_TOKEN"       : "==================================================",
+        "ACCESS_TOKEN_SECRET": "=================================================="}
 
-FILE_TARGET_USERS = "files/targetUsers.txt"
-FILE_FOLLOWED_USERS = "files/followedUsers.txt"
-FILE_STATS = "files/stats.txt"
-FILE_UNFOLLOWED_USERS = "files/unfollowedUsers.txt"
+# FILES
+FILE_TARGET_USERS = "files/targetUsers.txt"             # Stores the users you are going to follow
+FILE_FOLLOWED_USERS = "files/followedUsers.txt"         # Stores the users that have been followed
+FILE_STATS = "files/stats.txt"                          # Stores the stats of your twitter account
+FILE_UNFOLLOWED_USERS = "files/unfollowedUsers.txt"     # Stores the users that have been unfollowed
 
-ONE_DAY_MS = 86400000
-ONE_DAY_S = 86400
+# GENERAL
+TWITTER_USERNAME = "==========="                        # Your twitter username
+ONE_DAY_MS = 86400000                                   # One day in Milliseconds
+ONE_DAY_S = 86400                                       # On day in Seconds
 
-FOLLOWS_PER_DAY = 300
-UNFOLLOWS_PER_DAY = 300
-DAYS_TO_UNFOLLOW = 2
-
-TWITTER_USERNAME = "==========="
+# LIMITS
+FOLLOWS_PER_DAY = 300                                   # Users to follow per day
+UNFOLLOWS_PER_DAY = 300                                 # Users to unfollow per day
+DAYS_TO_UNFOLLOW = 2                                    # How many days to wait to unfollow a user
